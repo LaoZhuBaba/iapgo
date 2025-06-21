9996e0019bc851e3dde753628da3d9454ffeabec
David Liebert <david.liebert@kasna.com.au>
2025-06-21 13:25:59 +1200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # iapgo
-A configuration wrapper for launch Google Identity Aware Proxy (IAP) tunnels
+#### A configuration wrapper for launching Google Identity Aware Proxy (IAP) tunnels
+
+This tool assumes that you have installed the Google Cloud SDK and have
+logged in with sufficient privileges to connect via Identity Aware Proxy.
+
+N.B.  You must set your Application Default Credentials, either:
+```
+gcloud auth login [ACCOUNT] --update-adc
+```
+or
+```
+gcloud auth application-default login
+```
+See: https://cloud.google.com/docs/authentication/application-default-credentials
+
+You will need to know the name the target GCE instance, what project it
+is in, and the zone to which it is deployed.
+
+You could find this out with:
+
+```
+gcloud compute instances list [--project=my_project]
+```
+You  also need to ensure that the GCE instance is listening on the defined
+remote_port and typically you will need a VPC firewall rule to allow
+access to this TCP port from Google's IAP proxy server CIDR which is 35.235.240.0/20
+
+Unless your target GCE instance has multiple network interfaces *remote_nic*
+should always be set to *nic0*.
+
+The *exec* command is optional but is useful if you want to run a particular
+program when the tunnel starts.  For Linux/MacOS it is typically easiest
+to run *bash -c* followed by command that can include multiple words.  But
+you could run a command directly without *bash -c*, but then you would
+need to deal with any spaces yourself.  If you find this confusing then it
+is similar to Dockerfile, so read up on that.  I presume that on Windows *cmd /c* would
+work in a similar way.
+```
+Usage:
+iapgo [-c config_section] [-f config_file_name] [-v]
+
+-c string
+    select a non-default configuration file section (default "default")
+-f string
+    select a non-default configuration file (default "iapgo.yaml")
+-h  print a usage message
+-v  print debugging messages
+```
+
+Example configuration file:
+```
+default:
+  project_id: my-gcp-project
+  zone: us-central1-a
+  instance: my-jumpbox
+  local_port: 1234
+  remote_port: 1433
+  remote_nic: nic0
+  exec:
+    - bash
+    - "-c"
+    - sqlcmd -S tcp:localhost,1234 -U fred -No -d mydb -Q 'SELECT * FROM mytable'
+example:
+  project_id: my-gcp-project
+  zone: us-central1-a
+  instance: my-jumpbox
+  local_port: 1111
+  remote_port: 1111
+  remote_nic: nic0
+  exec:
+    - bash
+    - "-c"
+    - command and arguments
+```
---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log/slog"
+	"net"
+	"os"
+	"os/exec"
+	"os/signal"
+
+	config "github.com/LaoZhuBaba/iapgo/config"
+	tunnel "github.com/davidspek/go-iap-tunnel/pkg"
+	yaml "gopkg.in/yaml.v3"
+)
+
+const (
+	defaultConfigFileName = "iapgo.yaml"
+	defaultConfigSection  = "default"
+)
+
+func abnormalExit() {
+	flag.Usage()
+	os.Exit(1)
+}
+
+func main() {
+	// Trap if we are killed by Control-C or similar
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer stop()
+
+	helpPtr := flag.Bool("h", false, "print a usage message")
+	configSectionPtr := flag.String("c", defaultConfigSection, "select a non-default configuration file section")
+	configFilePtr := flag.String("f", defaultConfigFileName, "select a non-default configuration file")
+	verbosePtr := flag.Bool("v", false, "print debugging messages")
+
+	flag.Parse()
+
+	if *helpPtr {
+		flag.Usage()
+		fmt.Println("\nExample configuration file...")
+		fmt.Print(config.ExampleConfig)
+		return
+	}
+
+	var logLevel slog.LevelVar
+	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: &logLevel,
+	}))
+	slog.SetDefault(logger)
+
+	if *verbosePtr {
+		logLevel.Set(slog.LevelDebug)
+	}
+
+	yamlFile, err := os.ReadFile(*configFilePtr)
+	if err != nil {
+		logger.Error("failed to read YAML file with error", "error", err)
+		abnormalExit()
+	}
+
+	var cfgMap map[string]config.Config
+
+	err = yaml.Unmarshal(yamlFile, &cfgMap)
+	if err != nil {
+		logger.Error("Error unmarshalling YAML", "error", err)
+		abnormalExit()
+	}
+	cfg, ok := cfgMap[*configSectionPtr]
+	if !ok {
+		logger.Error("config section does not exist", "*configSectionPtr", *configSectionPtr)
+		abnormalExit()
+	}
+
+	logger.Debug("config", "cfgMap[*configSectionPtr]", cfgMap[*configSectionPtr])
+
+	ctx, cancel := context.WithCancel(ctx)
+
+	go func() {
+		logger.Debug("starting tunnel")
+		defer logger.Debug("ending tunnel")
+		startTunnel(ctx, cfg, logger)
+	}()
+
+	if len(cfg.Exec) > 0 {
+		go func() {
+			logger.Debug("running command")
+			defer logger.Debug("ending command")
+
+			runCmd(ctx, cfg.Exec, logger)
+			cancel()
+		}()
+
+	}
+	<-ctx.Done()
+}
+
+func runCmd(ctx context.Context, args []string, logger *slog.Logger) {
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	err := cmd.Run()
+	if err != nil {
+		logger.Error("failed to run command", "error", err)
+	}
+}
+
+func startTunnel(ctx context.Context, conf config.Config, logger *slog.Logger) {
+	go func() {
+		target := tunnel.TunnelTarget{
+			Project:   conf.ProjectID,
+			Zone:      conf.Zone,
+			Instance:  conf.Instance,
+			Port:      conf.RemotePort,
+			Interface: conf.RemoteNic,
+		}
+
+		listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", conf.LocalPort))
+		if err != nil {
+			logger.Error("failed to listen", "error", err)
+			abnormalExit()
+		}
+
+		manager := tunnel.NewTunnelManager(target, nil)
+
+		err = manager.Serve(context.Background(), listener)
+		if err != nil {
+			logger.Error("failed to start tunnel", "error", err)
+			abnormalExit()
+		}
+	}()
+	<-ctx.Done()
+}
